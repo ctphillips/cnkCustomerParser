a0fa9cac31cae3f550a3ec4ca7bc9a828a6129a9
Craig Phillips <cphillips.iphone@gmail.com>
2020-04-07 20:11:12 -0500

    adding expected export header for QBO customer list

--- customersQBO.py
+++ customersQBO.py
@@ -2,6 +2,10 @@ expectedHeader = ['Name', 'Company', 'Customer Type','Email', 'Phone',
     'Mobile', 'Fax', 'Website', 'Street', 'City','State', 'ZIP', 'Country',
     'Opening Balance', 'Date', 'Resale Number']
 
+expectedExportHeader = ['Customer', 'Company', 'Street Address', 'City',
+    'State', 'Country', 'ZIP', 'Phone', 'Email', 'Attachments',
+    'Open Balance', 'Notes']
+
 def removeDuplicates(listOfOrderedDict):
     listWithoutDuplicates = []
     for row in listOfOrderedDict:
